8df1e2bbfafb3de9a7bfbba924a80ffb5f42c029
xwh <1440325490@qq.com>
2022-03-06 17:27:16 +0800

    第一次提交

---
+++ 02-MyBatis-04-Placeholder/src/main/java/com/fc/dao/StudentDao.java
@@ -0,0 +1,16 @@
+package com.fc.dao;
+
+import com.fc.entity.Student;
+
+import java.util.List;
+
+public class StudentDao {
+    //查询学生指定年龄
+    public List<Student> findByAge(String age) {
+        return null;
+    }
+
+    public List<Student> findById(String id) {
+        return null;
+    }
+}
